dca64f47b745346eb49b9218c7b74a861372d266
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-10-26 07:52:49 -0500

    Avanza gema

--- bin/pruebaconsgema.sh
+++ bin/pruebaconsgema.sh
@@ -4,9 +4,10 @@
 rm msip-*.gem
 gem build
 mkdir -p x
+rm -rf x/*
 cd x
-rm -rf *
 tar xvf ../msip-*.gem
-cp ../test/dummy/.env test/dummy/
+tar xvfz data.tar.gz
+cp ../test/dummy/.env ./test/dummy/
 bin/gc.sh
 
