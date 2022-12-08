8b5edc23ef76e877f36a992e200c36c5bbfbbab8
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-12-08 11:17:39 -0500

    Renombra indices

---
+++ db/migrate/20221201154025_renombra_indices.rb
@@ -0,0 +1,29 @@
+class RenombraIndices < ActiveRecord::Migration[7.0]
+
+  include Msip::SqlHelper
+
+  IND=[
+    ['sip_busca_mundep', 'msip_busca_mundep'],
+    ['sip_nombre_ubicacionpre_b', 'msip_nombre_ubicacionpre_b'],
+    ['sip_persona_anionac_ind', 'msip_persona_anionac_ind'],
+    ['sip_persona_sexo_ind', 'msip_persona_sexo_ind'],
+    ['trelacion_pkey', 'msip_trelacion_pkey'],
+    ['tclase_pkey', 'msip_tclase_pkey'],
+  ]
+
+  def up
+    IND.each do |i|
+      if existe_índice_pg?(i[0])
+        renombrar_índice_pg(i[0], i[1])
+      end
+    end
+  end
+
+  def down
+    IND.reverse.each do |i|
+      if existe_índice_pg?(i[1])
+        renombrar_índice_pg(i[1], i[0])
+      end
+    end
+  end
+end
