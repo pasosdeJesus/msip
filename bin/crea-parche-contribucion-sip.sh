999528ab2edccc235757b09e7877fa3658884f5e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-12-28 22:27:23 -0500

    Actualiza

--- bin/crea-parche-contribucion-sip.sh
+++ bin/crea-parche-contribucion-sip.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+if (test "$DIRO" = "") then {
+  export DIRO=sip
+} fi;
 contribucion=$1
 if (test "$contribucion" = "") then {
   echo "Falta condensado de contribución"
   exit 1;
 } fi;
-git --git-dir=../sip/.git format-patch -k -1 --stdout $contribucion
+git --git-dir=../$DIRO/.git format-patch -k -1 --stdout $contribucion | sed -e "s/sip/msip/g;s/Sip/Msip/g;"
