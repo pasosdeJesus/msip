b1dcaa4d89394aa62db91ee10932787e226ef426
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-02 18:27:44 -0500

    depura

--- lib/msip/concerns/controllers/anexos_controller.rb
+++ lib/msip/concerns/controllers/anexos_controller.rb
@@ -54,7 +54,7 @@ module Msip
               puts "OJO anexo.id=#{@anexo.id}"
               ruta = @anexo.adjunto_file_name
               puts "OJO ruta=#{ruta}"
-              puts "OJO Msip.ruta_anexo=#{Msip.ruta_anexo.to_s}"
+              puts "OJO Msip.ruta_anexo=#{Msip.ruta_anexos.to_s}"
               pdfp=sprintf(Msip.ruta_anexos.to_s + "/%d_%s", @anexo.id.to_i, 
                            File.basename(ruta))
               ruta_im = ''
