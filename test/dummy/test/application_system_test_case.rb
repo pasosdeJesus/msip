a0aade78cbca6ea767c4e0a42bb42f2bbfaa0c1a
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-01-05 00:37:22 -0500

    prueba para gitlab

--- test/dummy/test/application_system_test_case.rb
+++ test/dummy/test/application_system_test_case.rb
@@ -5,7 +5,8 @@ require "capybara/cuprite"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.javascript_driver = :cuprite
-  puts "CHROME_URL=#{ENV.fetch("CHROME_URL", nil)}"
+  @elurl = ENV.fetch("CHROME_URL", "http://chrome:4444/wd/hub")
+  puts "@elur=#{@elurl}"
   Capybara.register_driver(:cuprite) do |app|
     #if File.exist?("/.dockerenv")
       Capybara::Cuprite::Driver.new(
@@ -13,7 +14,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
         window_size: [1200, 800],
         browser_options: { "no-sandbox": nil },
         inspector: true,
-        url: ENV.fetch("CHROME_URL", nil),
+        url: @elurl
       )
     #else
     #  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800])
