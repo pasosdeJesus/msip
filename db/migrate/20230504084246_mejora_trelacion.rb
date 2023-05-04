f7091a3a9c61c0a5c0e11f5f562c693222cd0659
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-05-04 03:54:01 -0500

    Mejora trelacion. Avanza https://gitlab.com/pasosdeJesus/si_fasol/-/issues/149

---
+++ db/migrate/20230504084246_mejora_trelacion.rb
@@ -0,0 +1,19 @@
+class MejoraTrelacion < ActiveRecord::Migration[7.0]
+  def up
+    execute <<-SQL
+      UPDATE msip_trelacion SET nombre='MADRASTRA/PADRASTRO'
+        WHERE id = 'PD';
+      UPDATE msip_trelacion SET inverso='SI'
+        WHERE id = 'SI';
+    SQL
+  end
+  def down
+    execute <<-SQL
+      UPDATE msip_trelacion SET nombre='MADRASTRA (PADRASTRO)'
+        WHERE id = 'PD';
+      UPDATE msip_trelacion SET inverso=NULL
+        WHERE id = 'SI';
+    SQL
+
+  end
+end
