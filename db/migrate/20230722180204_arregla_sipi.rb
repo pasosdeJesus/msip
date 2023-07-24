549f7acdfa1bf036bb632d969094848bd472a29b
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-07-24 14:53:58 -0400

    Arreglos a datos

--- db/migrate/20230722180204_arregla_sipi.rb
+++ db/migrate/20230722180204_arregla_sipi.rb
@@ -3,12 +3,13 @@ class ArreglaSipi < ActiveRecord::Migration[7.0]
     execute <<-SQL
       UPDATE msip_municipio SET
       nombre='Sipí' WHERE id=1207;
+
+      UPDATE msip_clase SET
+      nombre='Sipirra' WHERE id=1652;
+      UPDATE msip_clase SET
+      nombre='Sipí' WHERE id=5448;
     SQL
   end
   def down
-    execute <<-SQL
-      UPDATE msip_municipio SET
-      nombre='Msipí' WHERE id=1207;
-    SQL
   end
 end
