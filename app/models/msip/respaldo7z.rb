c95727016f9175a871e8fb8cc8d7aa3b47b18ee6
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-09-24 20:00:51 -0500

    Refactoriza modelos para que usen Concern. https://gitlab.com/pasosdeJesus/sivel2_gen/-/issues/554

--- app/models/msip/respaldo7z.rb
+++ app/models/msip/respaldo7z.rb
@@ -1,22 +1,10 @@
 # frozen_string_literal: true
 
+require "msip/concerns/models/respaldo7z"
+
 module Msip
   class Respaldo7z
     include ActiveModel::Model
-
-    attr_accessor :clave7z
-
-    validates :clave7z, presence: true, confirmation: true, format: {
-      with: /\A[-A-Za-z0-9{}*.,:;+@^~<>#_!$%&()=?]*\z/,
-      message: "sólo letras, dígitos y los siguientes caracteres especiales -{}*.,:;+@^~<>#_!$%&()=?",
-    }
-    validates :clave7z_confirmation, presence: true
-
-    validate :existe_7z
-    def existe_7z
-      if Msip::TareasrakeHelper.which("7z").nil?
-        errors.add(:base, "No se encontró el programa 7z en el servidor")
-      end
-    end
+    include Msip::Concerns::Models::Respaldo7z
   end
 end
---
+++ lib/msip/concerns/models/respaldo7z.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Msip
+  module Concerns
+    module Models
+      module Respaldo7z
+        extend ActiveSupport::Concern
+        included do
+          include ActiveModel::Model
+
+          attr_accessor :clave7z
+
+          validates :clave7z, presence: true, confirmation: true, format: {
+            with: /\A[-A-Za-z0-9{}*.,:;+@^~<>#_!$%&()=?]*\z/,
+            message: "sólo letras, dígitos y los siguientes caracteres especiales -{}*.,:;+@^~<>#_!$%&()=?",
+          }
+          validates :clave7z_confirmation, presence: true
+
+          validate :existe_7z
+          def existe_7z
+            if Msip::TareasrakeHelper.which("7z").nil?
+              errors.add(:base, "No se encontró el programa 7z en el servidor")
+            end
+          end
+
+        end
+      end
+    end
+  end
+end
