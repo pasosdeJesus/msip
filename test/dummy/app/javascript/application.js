40ebdd5de75ca61c660dea220cfc94a1c46a0b9f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2023-11-16 11:27:44 -0500

    Usa defer e inicializa mejor rails/ujs

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -5,7 +5,7 @@ console.log('Hola Mundo desde ESM')
 import Rails from "@rails/ujs";
 if (typeof window.Rails == 'undefined') {
   Rails.start();
-  window.Rails = Rails
+  window.Rails = Rails;
 }
 
 import "@hotwired/turbo-rails"
