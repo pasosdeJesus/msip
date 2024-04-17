13e0107d1417108110a64dd4ff0940727b54c7ea
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-04-16 21:47:59 -0500

    Msip__Motor usable en controladores stimulus y en javascript global

--- test/dummy/app/javascript/application.js
+++ test/dummy/app/javascript/application.js
@@ -29,6 +29,9 @@ window.configuracionTomSelect = {
   }
 }
 
+import Msip__Motor from './controllers/msip/motor.js'
+window.Msip__Motor = Msip__Motor
+
 import {AutocompletaAjaxExpreg} from '@pasosdejesus/autocompleta_ajax'
 window.AutocompletaAjaxExpreg = AutocompletaAjaxExpreg
 
