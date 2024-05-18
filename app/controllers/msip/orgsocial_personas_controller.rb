91e938d5b3fc63ad4786f773eb268f26a61d8ddc
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-05-18 06:09:23 -0500

    Faltaba de orgsocial. Para https://gitlab.com/pasosdeJesus/sivel2_gen/-/issues/651

---
+++ app/controllers/msip/orgsocial_personas_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "msip/concerns/controllers/orgsocial_personas_controller"
+
+module Msip
+  class OrgsocialPersonasController < ApplicationController
+    before_action :preparar_orgsocial_persona
+    load_and_authorize_resource class: Msip::Orgsocial
+
+    include Msip::Concerns::Controllers::OrgsocialPersonasController
+  end
+end
