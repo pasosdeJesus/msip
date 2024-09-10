39ec2b909dae0bfc9eb526f3631b7fbf7d23912e
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-10 10:20:55 -0400

    doc relaciones

--- app/models/msip/orgsocial_sectororgsocial.rb
+++ app/models/msip/orgsocial_sectororgsocial.rb
@@ -3,8 +3,7 @@
 require "msip/concerns/models/orgsocial_sectororgsocial"
 
 module Msip
-  # Implementa relación n-n entre organización social y sector de organización
-  # social
+  # Relación n:n entre organización social y sector de organización social
   class OrgsocialSectororgsocial < ActiveRecord::Base
     include Msip::Concerns::Models::OrgsocialSectororgsocial
   end
