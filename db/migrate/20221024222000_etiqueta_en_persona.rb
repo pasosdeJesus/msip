8552be062b10ac42050a2b2e3a050051cf0b3aa7
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-09-13 16:38:19 -0500

    Orden

--- db/migrate/20221024222000_etiqueta_en_persona.rb
+++ db/migrate/20221024222000_etiqueta_en_persona.rb
@@ -1,15 +1,20 @@
 class EtiquetaEnPersona < ActiveRecord::Migration[7.0]
-  def change
-    create_table :msip_etiqueta_persona do |t|
-      t.integer :etiqueta_id, null: false
-      t.integer :persona_id, null: false
-      t.integer :usuario_id, null: false
-      t.date :fecha, null: false
-      t.string :observaciones, limit: 5000
-      t.timestamps
+  def up
+    if !table_exist?(:msip_etiqueta_persona)
+      create_table :msip_etiqueta_persona do |t|
+        t.integer :etiqueta_id, null: false
+        t.integer :persona_id, null: false
+        t.integer :usuario_id, null: false
+        t.date :fecha, null: false
+        t.string :observaciones, limit: 5000
+        t.timestamps
+      end
+      add_foreign_key :msip_etiqueta_persona, :msip_etiqueta, column: :etiqueta_id
+      add_foreign_key :msip_etiqueta_persona, :msip_persona, column: :persona_id
+      add_foreign_key :msip_etiqueta_persona, :usuario, column: :usuario_id
     end
-    add_foreign_key :msip_etiqueta_persona, :msip_etiqueta, column: :etiqueta_id
-    add_foreign_key :msip_etiqueta_persona, :msip_persona, column: :persona_id
-    add_foreign_key :msip_etiqueta_persona, :usuario, column: :usuario_id
+  end
+  def down
+    drop_table :msip_etiqueta_persona
   end
 end
