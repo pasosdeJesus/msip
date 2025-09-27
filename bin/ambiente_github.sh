052d6df0bb00f16269004a17d0854fb69ee342c4
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-09-27 05:18:11 -0500

    Actualiza gh

--- bin/ambiente_github.sh
+++ bin/ambiente_github.sh
@@ -1,61 +1,75 @@
 #!/bin/bash
 
-# Este script pepara un codspace de github con PostgreSQL y la misma 
-# configuración de un workflow Github Action para este repositorio
-# (see .github/workflows/rubyonrails.yml)
+# Este script pepara un codspace/action de github con locale, paquetes 
+# (e.g PostgreSQL, # PostGIS), base de datos y variables de ambiente 
+# para correr la aplicación de prueba y/o las pruebas
 
 # pdftoppm en poppler-utils
-sudo apt install poppler-utils
+echo "Ejecutando ambiente_github.sh"
+sudo apt update
+sudo apt install -y poppler-utils
+sudo locale-gen es_CO.UTF-8 && sudo update-locale
 
-# Instala PostgreSQL 16 y las librerías requeridas
-if (test ! -f /etc/postgresql/16/main/pg_hba.conf) then {
-	wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-	echo 'deb http://apt.postgresql.org/pub/repos/apt/ noble-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+# Instala PostgreSQL 17 y PostGIS 3 y las librerías requeridas
+if (test ! -f /etc/postgresql/17/main/pg_hba.conf) then {
+	wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/postgresql.asc
+	echo 'deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/postgresql.asc] https://apt.postgresql.org/pub/repos/apt/ noble-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
 	sudo apt update
-	sudo apt install -y postgresql-16 postgresql-client-16
-  sudo apt-get update
-  sudo apt-get install postgis postgresql-16-postgis-3
-
-	sudo locale-gen es_CO.UTF-8 && sudo update-locale
-
-	sudo pg_createcluster 16 main --start
-	sudo sed -i "s|local.*all.*all.*peer|local all all md5|g" /etc/postgresql/16/main/pg_hba.conf
+	sudo apt install -y postgresql-17 postgresql-client-17
+	sudo apt install -y postgis postgresql-17-postgis-3
+	sudo pg_createcluster 17 main --start
+	sudo sed -i "s|local.*all.*all.*peer|local all all md5|g" /etc/postgresql/17/main/pg_hba.conf
+	sudo cat /etc/postgresql/17/main/pg_hba.conf
 } fi;
+echo "Reiniciando postgresql"
+sudo service postgresql restart
+echo "PostgreSQL reiniciado"
 
-sudo service postgresql status | grep online
-if (test "$?" != "0") then {
-	# Start PostgreSQL service
-	sudo service postgresql start
-} fi;
+psql --version
 
 # Crea usuario y base de datos como usuario postgres
+echo "Creando usuario"
 sudo su - postgres -c "createuser -s rails"
+echo "Cambiando clave"
 sudo su - postgres -c "psql -c \"ALTER USER rails WITH PASSWORD 'password';\""
+echo "Facilitando uso"
 echo "*:*:*:rails:password" >> ~/.pgpass
 chmod 0600 ~/.pgpass
-createdb -U rails rails_test
+echo ":::: cat ~/.pgpass::::"   
+cat ~/.pgpass     
+echo "Creando base"
+sudo su - postgres -c "createdb -O rails rails_test"
+echo "Creada"
 
 # Configura el ambiente
-cd "$(dirname "$0")/../test/dummy" || exit 1
+cd "$(dirname "$0")/.." || exit 1
+if (test -d test/dummy) then {
+  cd test/dummy
+} fi;
+pwd
 cp .env.github .env
+echo "::::.env antes de modificar es::::"
+cat .env
 
 # Modifica el archivo .env con rutas locales y configuraciones
 DIRAP=$(pwd)
-sed -i "s|/home/runner/work/msip/msip/|${DIRAP}/|g" .env
-sed -i "s|export BD_SERVIDOR=.*|export BD_SERVIDOR=localhost|" .env
-sed -i "s|export DATABASE_URL=.*|export
-DATABASE_URL=\"postgres://rails:password@localhost:5432/rails_test\"|" .env
+sed -i "s|export DIRAP=.*|export DIRAP=${DIRAP}/|g" .env
+#sed -i "s|export BD_SERVIDOR=.*|export BD_SERVIDOR=localhost|g" .env
 echo "export RAILS_ENV=test" >> .env
+
+echo "::::.env tras modificacion es es::::"
+cat .env
 
 # Ejecuta el archivo con variables de ambiente
 source .env
 
-bundle
-
 echo "¡Ambiente configurado con exito!"
+echo "Directorio de la aplicaciónde datos: $DIRAP"
 echo "Base de datos: $BD_PRUEBA"
 echo "Usuario: $BD_USUARIO"
-echo "URL de la base de datos: $DATABASE_URL"
+echo "host: $BD_SERVIDOR"
 echo "Localización del archivo de ambiente: $DIRAP/.env"
 
+# bundle
 # RAILS_ENV=test bin/rails dbconsole
+
