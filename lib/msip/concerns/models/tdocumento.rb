f50410eb79ecf8b76b0688cc4539e85c9215e1e2
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-10-03 15:46:44 +0000

    Sigals unicas

--- lib/msip/concerns/models/tdocumento.rb
+++ lib/msip/concerns/models/tdocumento.rb
@@ -9,7 +9,7 @@ module Msip
         include Msip::Basica
         included do
           self.table_name = "msip_tdocumento"
-          validates :sigla, length: { maximum: 100 }, presence: true
+          validates :sigla, length: { maximum: 100 }, presence: true, uniqueness: true
           validates :formatoregex, length: { maximum: 500 }
         end
       end
