6597849212f96a3238f7e56afc96835d1911d8f1
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-10-05 18:39:04 -0500

    Improves script

--- bin/jaladegithub_empujaagitlab.sh
+++ bin/jaladegithub_empujaagitlab.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+branch=`git rev-parse --abbrev-ref HEAD`
+echo "branch=$branch"
 git remote remove origin
 git remote add origin git@github.com:pasosdeJesus/msip
-git pull origin main
+git pull origin $branch
 git remote remove origin
 git remote add origin git@gitlab.com:pasosdeJesus/msip
+git branch --set-upstream-to=origin/$branch $branch
 git push
