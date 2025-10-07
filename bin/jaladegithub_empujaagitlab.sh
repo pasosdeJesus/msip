d95caf0e2631295bd541c6146520dfe60960b53f
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-10-07 04:26:24 -0500

    doc

--- bin/jaladegithub_empujaagitlab.sh
+++ bin/jaladegithub_empujaagitlab.sh
@@ -7,5 +7,5 @@ git remote add origin git@github.com:pasosdeJesus/msip
 git pull origin $branch
 git remote remove origin
 git remote add origin git@gitlab.com:pasosdeJesus/msip
-git branch --set-upstream-to=origin/$branch $branch
-git push
+git push origin $branch
+#git branch --set-upstream-to=origin/$branch $branch
